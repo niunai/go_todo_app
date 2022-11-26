050e0de31e765f02154093031ad8f5b427af5a46
niunai <shibah@gmail.com>
2022-11-26 11:04:48 +0900

    the last

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,18 @@ RUN go mod download
 COPY . .
 RUN go build -trimpath -ldflags "-w -s" -o app
 
+# -trimpath
+#         remove all file system paths from the resulting executable.
+#         Instead of absolute file system paths, the recorded file names
+#         will begin either a module path@version (when using modules),
+#         or a plain import path (when using the standard library, or GOPATH).
+
+# You will get the smallest binaries if you compile with -ldflags '-w -s'.
+# The -w turns off DWARF debugging information: you will not be able to use gdb on the binary to look at specific functions or set breakpoints or get stack traces, because all the metadata gdb needs will not be included. You will also not be able to use other tools that depend on the information, like pprof profiling.
+# The -s turns off generation of the Go symbol table: you will not be able to use go tool nm to list the symbols in the binary. strip -s is like passing -s to -ldflags but it doesn't strip quite as much. go tool nm might still work after strip -s. I am not completely sure.
+#
+# $ go tool link
+
 # ---------------------------------------------------
 
 FROM debian:bullseye-slim as deploy
--- mux.go
+++ mux.go
@@ -1,13 +1,73 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"net/http"
 
-func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+	"github.com/niunai/go_todo_app/auth"
+	"github.com/niunai/go_todo_app/clock"
+	"github.com/niunai/go_todo_app/config"
+	"github.com/niunai/go_todo_app/handler"
+	"github.com/niunai/go_todo_app/service"
+	"github.com/niunai/go_todo_app/store"
+)
+
+func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), error) {
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		// 静的解析のエラーを回避するため明示的に戻り値を捨てている
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
-	return mux
+	v := validator.New()
+	db, cleanup, err := store.New(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
+	}
+	clocker := clock.RealClocker{}
+	r := store.Repository{Clocker: clocker}
+	rcli, err := store.NewKVS(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
+	}
+	jwter, err := auth.NewJWTer(rcli, clocker)
+	if err != nil {
+		return nil, cleanup, err
+	}
+	ru := &handler.RegisterUser{
+		Service:   &service.RegisterUser{DB: db, Repo: &r},
+		Validator: v,
+	}
+	mux.Post("/register", ru.ServeHTTP)
+	l := &handler.Login{
+		Service: &service.Login{
+			DB:             db,
+			Repo:           &r,
+			TokenGenerator: jwter,
+		},
+		Validator: v,
+	}
+	mux.Post("/login", l.ServeHTTP)
+	at := &handler.AddTask{
+		Service:   &service.AddTask{DB: db, Repo: &r},
+		Validator: v,
+	}
+	lt := &handler.ListTask{
+		Service: &service.ListTask{DB: db, Repo: &r},
+	}
+	mux.Route("/tasks", func(r chi.Router) {
+		r.Use(handler.AuthMiddleware(jwter))
+		r.Post("/", at.ServeHTTP)
+		r.Get("/", lt.ServeHTTP)
+	})
+	mux.Route("/admin", func(r chi.Router) {
+		r.Use(handler.AuthMiddleware(jwter), handler.AdminMiddleware)
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			_, _ = w.Write([]byte(`{"message": "admin only"}`))
+		})
+	})
+
+	return mux, cleanup, nil
 }
